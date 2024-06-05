f7b6c010e6da26629fb193214ee45719a807523b
visw20 <162998373+visw20@users.noreply.github.com>
2024-06-05 11:13:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import streamlit as st
 
-st.write("Hello world!!!!")
-num=int(input("enter your number: "))
-if num%2==0:
-  print("even")
-else:
-  print("odd")
+def main():
+    st.title("Simple User Input Checker")
+
+    # Text input widget
+    user_input = st.text_input("Enter some text:")
+
+    # Check if user input is not empty
+    if user_input:
+        st.write("You entered:", user_input)
+    else:
+        st.write("Please enter some text.")
+
+if __name__ == "__main__":
+    main()
+
 
